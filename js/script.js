310edfa772eaf2ec46fe4cfe020eea60b44b6342
Rafał Pietrzak <rpietrzak@pixelage.pl>
2019-12-09 11:17:42 +0100

    Add game conditions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let playerMove = '';
+let computerMove = '';
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = playerInput == '1' ? 'kamień' : 
+             playerInput == '2' ? 'papier' :  
+             playerInput == '3' ? 'nożyce' : 
+             'Podaj poprawną wartość 1, 2, 3.';
+
+computerMove = randomNumber == '1' ? 'kamień' : 
+               randomNumber == '2' ? 'papier' :  
+               randomNumber == '3' ? 'nożyce' : 
+               'Błąd #^*^@!';
+
+printMessage(
+    playerMove == computerMove ? 'Remis!' :
+    playerMove == 'kamień' && computerMove == 'papier' ? 'Twój ruch to ' + playerMove + '. Komputer wylosował ' + computerMove + '. Przegrałeś :(' :
+    playerMove == 'kamień' && computerMove == 'nożyce' ? 'Twój ruch to ' + playerMove + '. Komputer wylosował ' + computerMove + '. Wygrałeś !!!' :
+    playerMove == 'nożyce' && computerMove == 'papier' ? 'Twój ruch to ' + playerMove + '. Komputer wylosował ' + computerMove + '. Wygrałeś !!!' :
+    playerMove == 'nożyce' && computerMove == 'kamień' ? 'Twój ruch to ' + playerMove + '. Komputer wylosował ' + computerMove + '. Przegrałeś :(' :
+    playerMove == 'papier' && computerMove == 'nożyce' ? 'Twój ruch to ' + playerMove + '. Komputer wylosował ' + computerMove + '. Przegrałeś :(' :
+    playerMove == 'papier' && computerMove == 'kamień' ? 'Twój ruch to ' + playerMove + '. Komputer wylosował ' + computerMove + '. Wygrałeś !!!' :
+    'Błąd #^*^@!'
+);
