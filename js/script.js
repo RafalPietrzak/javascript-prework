e2968cfe38dd25df418ca3d8282d21bb0f68fe40
Rafał Pietrzak <rpietrzak@pixelage.pl>
2019-12-12 15:59:58 +0100

    Add more styles and fix bag

--- js/script.js
+++ js/script.js
@@ -1,97 +1,3 @@
-/*function playGame(playerInput) {
-    clearMessages();
-
-    function getMoveName(moveID) {
-        if (moveID == '1'){
-             return 'kamień';
-        } else if (moveID == '2') {
-            return 'papier';
-        } else if (moveID == '3') {
-            return 'nożyce'; 
-        } else {
-            return 'error';
-        } 
-    }
-
-    function checkResult(playerMove, computerMove) {
-        const result = {
-            'draw': 'Remis!',
-            'playerWin': 'Wygrałeś !!!',
-            'computerWin': 'Przegrałeś :[',
-            'printResult': function (playerMove, computerMove, result) {
-                    printMessage('Twój ruch to ' + playerMove + 
-                    '. Komputer wylosował ' + computerMove + 
-                    '. ' + result); 
-                }
-            };
-        
-        if (playerMove == computerMove) {
-            result.printResult(playerMove, computerMove, result.draw);
-        } else if (playerMove == 'kamień' && computerMove == 'papier'){
-            result.printResult(playerMove, computerMove, result.computerWin);
-        } else if (playerMove == 'kamień' && computerMove == 'nożyce'){
-            result.printResult(playerMove, computerMove, result.playerWin);
-        } else if (playerMove == 'nożyce' && computerMove == 'papier'){
-            result.printResult(playerMove, computerMove, result.playerWin);
-        } else if (playerMove == 'nożyce' && computerMove == 'kamień'){
-            result.printResult(playerMove, computerMove, result.computerWin);
-        } else if (playerMove == 'papier' && computerMove == 'nożyce'){
-            result.printResult(playerMove, computerMove, result.computerWin);
-        } else if (playerMove == 'papier' && computerMove == 'kamień'){
-            result.printResult(playerMove, computerMove, result.playerWin);
-        } else {
-            console.log('ERROR')    
-        }         
-    }
-
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    let playerMove = getMoveName(playerInput);
-    let computerMove = getMoveName(randomNumber);
-    checkResult(playerMove, computerMove);
-}
-
-document.getElementById('play-rock').addEventListener('click', function () {
-    playGame('1');
-});
-document.getElementById('play-paper').addEventListener('click', function () {
-    playGame('2');
-});
-document.getElementById('play-scissors').addEventListener('click', function () {
-    playGame('3');
-});
-
-let o = {};
-o.a = 10;
-let p = Object.create(o);
-console.log('1 :: a:' + p.a + ', b:' + p.b); // 1 :: a:10, b:undefined
-console.log(toString(p)); //{} b: 12 __proto__: a: 10 b: 13
-o.b = 11;
-console.log('2 :: a:' + p.a + ', b:' + p.b); // 2 :: a:10, b:11
-console.log(p); //{} b: 12 __proto__: a: 10 b: 13
-p.b = 12;
-console.log('3 :: a:' + p.a + ', b:' + p.b); // 3 :: a:10, b:12
-console.log(p); //{} b: 12 __proto__: a: 10 b: 13
-o.b = 13;
-console.log('4 :: a:' + p.a + ', b:' + p.b); // 4 :: a:10, b:12
-console.log(p); //{} b: 12 __proto__: a: 10 b: 13
-
-o.c = 10;
-o.d = function (value) {
-    o.c = value;
-} 
-
-q = Object.create(o);
-console.log('q.c: ' + q.c + ', o.c:' + o.c);
-
-q.d(20);
-console.log('q.c: ' + q.c + ', o.c:' + o.c);
-
-o.d(30);
-console.log('q.c: ' + q.c + ', o.c:' + o.c);
-
-q.c = 10;
-console.log('q.c: ' + q.c + ', o.c:' + o.c);
-*/
 const game = function (){
      
     //id = css id, click = function run on click
@@ -100,8 +6,7 @@ const game = function (){
             'id' : 'player-rock',
             'element' : {},
             'click' : function () { 
-                switchButton();
-                updateFilds.playerMove.update(moveIcon.rock);
+                gameLogic(moves.rock);
             },
             'disabled' : true 
         },
@@ -109,8 +14,7 @@ const game = function (){
             'id' : 'player-paper',
             'element' : {},
             'click' : function () {
-                switchButton();
-                updateFilds.playerMove.update(moveIcon.paper);
+                gameLogic(moves.paper);
             },
             'disabled' : true
         },
@@ -118,8 +22,7 @@ const game = function (){
             'id' : 'player-scissors',
             'element' : {},
             'click' : function () {
-                switchButton();
-                updateFilds.playerMove.update(moveIcon.scissors);
+                gameLogic(moves.scissors);
             },
             'disabled' : true
         },
@@ -128,8 +31,9 @@ const game = function (){
             'element' : {},
             'click' : function () { 
                 this.innerHTML = 'Następna runda';
-                updateFilds.playerMove.update(moveIcon.newRound);
-                updateFilds.computerMove.update(moveIcon.newRound);
+                updateFilds.playerMove.update(moves.noMove.icon);
+                updateFilds.computerMove.update(moves.noMove.icon);
+                updateFilds.roundResult.update("");
                 switchButton();
                 incrementFilds.roundNumber.add()
             },
@@ -137,12 +41,74 @@ const game = function (){
         }
     }
     
+    const gameLogic = function(playerMove){
+        const getMoveByValue = function (value) {
+            if (value == '1'){
+                 return moves.rock;
+            } else if (value == '2') {
+                return moves.paper;
+            } else if (value == '3') {
+                return moves.scissors; 
+            } else {
+                return moves.noMove;
+            } 
+        }
+
+        const computerMove =  getMoveByValue(Math.floor(Math.random() * 3 + 1));
+        updateFilds.computerMove.update(computerMove.icon);
+        updateFilds.playerMove.update(playerMove.icon);
+        switchButton();
+        
+        if (playerMove.value == computerMove.value) {
+            updateFilds.roundResult.update(resultLabel.draw);
+        } else if (playerMove.value == moves.rock.value //p:rock c:paper 
+            && computerMove.value == moves.paper.value){
+               updateFilds.roundResult.update(resultLabel.playerFail);
+                incrementFilds.computerScore.add();
+        } else if (playerMove.value == moves.rock.value  //p:rock c:scissors
+            && computerMove.value == moves.scissors.value){
+                updateFilds.roundResult.update(resultLabel.playerWin);
+                incrementFilds.playerScore.add();
+        } else if (playerMove.value == moves.paper.value //p: paper c:rock 
+            && computerMove.value == moves.rock.value){
+                updateFilds.roundResult.update(resultLabel.playerWin);
+                incrementFilds.playerScore.add();
+        } else if (playerMove.value == moves.paper.value //p: paper c:scissors
+            && computerMove.value == moves.scissors.value){
+                updateFilds.roundResult.update(resultLabel.playerFail);
+                incrementFilds.computerScore.add();
+        } else if (playerMove.value == moves.scissors.value //p: scissors c:rock 
+            && computerMove.value == moves.rock.value){
+                updateFilds.roundResult.update(resultLabel.playerFail);
+                incrementFilds.computerScore.add();
+        } else if (playerMove.value == moves.scissors.value //p:scissors c:paper
+            && computerMove.value == moves.paper.value){
+                updateFilds.roundResult.update(resultLabel.playerWin);
+                incrementFilds.playerScore.add();
+        } else {
+            console.log('ERROR')    
+        }         
+    }
+
+    // Disabled and abled buttons
     const switchButton = function () {
         for(key of Object.keys(buttons)){
             buttons[key].disabled = !buttons[key].disabled; 
             buttons[key].element.disabled = buttons[key].disabled; 
         }
     }
+
+    //Get element and add increment function '.add()'
+    const addIncrementFunction = function () {
+        for(let key of Object.keys(incrementFilds)){
+            let fild = incrementFilds[key];
+            fild.element = document.getElementById(fild.id);
+            fild.add = function () {
+                this.state = this.state + 1;
+                this.element.innerHTML = '' + this.state;
+            } 
+        }
+    } 
 
     //Key - css id filds, value element and increment function
     const incrementFilds = {
@@ -188,25 +154,25 @@ const game = function (){
         'draw' : 'Remis!'
     }
 
-    const moveIcon = {
-        'newRound' : '<i class="fas fa-spinner player__move-icon"></i>',
-        'rock' : '<i class="fas fa-hand-rock player__move-icon"></i>',
-        'paper' : '<i class="fas fa-hand-paper player__move-icon"></i>',
-        'scissors' : '<i class="fas fa-hand-scissors player__move-icon"></i>',
-    }
-    
-    //Get element and add increment function '.add()'
-    const addIncrementFunction = function () {
-        for(let key of Object.keys(incrementFilds)){
-            let fild = incrementFilds[key];
-            fild.element = document.getElementById(fild.id);
-            fild.add = function () {
-                this.state = this.state + 1;
-                this.element.innerHTML = '' + this.state;
-            } 
+    const moves = {
+        'noMove' : {
+            'value' : null,
+            'icon' : '<i class="fas fa-spinner player__move-icon"></i>'
+        },
+        'rock' : {
+            'value' : '1',
+            'icon' : '<i class="fas fa-hand-rock player__move-icon"></i>'
+        },
+        'paper' : {
+            'value' : '2',
+            'icon' : '<i class="fas fa-hand-paper player__move-icon"></i>'
+        },
+        'scissors' : {
+            'value' : '3',
+            'icon' : '<i class="fas fa-hand-scissors player__move-icon"></i>'
         }
-    } 
-    
+    }
+
     //Get element and add listeners to all button
     const addListeners = function () {
         for(let key of Object.keys(buttons)){
